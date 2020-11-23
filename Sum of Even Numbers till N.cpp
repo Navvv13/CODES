10e8e830be337ce75f95fd9dbcd07e77fe1341e2
Navneet Kaur Assi <65205930+Navvv13@users.noreply.github.com>
2020-11-23 06:13:51 +0530

    Add files via upload

---
+++ Sum of Even Numbers till N.cpp
@@ -0,0 +1,22 @@
+ #include<iostream>
+using namespace std;
+
+
+int main(){
+
+  int N;
+    cin>>N;
+    cout<<endl;
+    int i=0;
+    int a=2;
+    while(a<=N){
+        i=a+i;
+    a=a+2;
+    
+    } 
+    cout<<i<<endl;
+    return 0;
+}
+
+
+
