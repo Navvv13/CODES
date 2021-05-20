8b368e0aef51e77f491a56e921157a62ec893fed
Navneet Kaur Assi <65205930+Navvv13@users.noreply.github.com>
2021-05-20 20:41:18 +0530

    Create TriangularStarPattern.cpp

---
+++ TriangularStarPattern.cpp
@@ -0,0 +1,28 @@
+/* Print the following pattern for the given N number of rows.
+Pattern for N = 4
+*
+**
+***
+****
+
+
+*/
+
+#include<iostream>
+using namespace std;
+ int main()
+ {
+     int n;
+     cin>>n;
+     for(int i=0;i<n;i++)
+     {
+         for(int j=0;j<=i;j++)
+         {
+             cout<<"*";
+             
+         }
+         cout<<endl;
+     }
+     
+     return 0;
+ }
