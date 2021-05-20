3a1131b1102cd665fc658f1d1f8166b2ba1aac85
Navneet Kaur Assi <65205930+Navvv13@users.noreply.github.com>
2021-05-20 20:50:33 +0530

    Create ReverseNumberPattern.cpp

---
+++ ReverseNumberPattern.cpp
@@ -0,0 +1,27 @@
+/*
+Print the following pattern for the given N number of rows.
+Pattern for N = 4
+1
+21
+321
+4321
+*/
+
+#include<iostream>
+using namespace std;
+ int main()
+ {
+     int n;
+     cin>>n;
+     for(int i=1;i<=n;i++)
+     {
+         for(int j=i;j>=1;j--)
+         {
+             cout<<j;
+             
+         }
+         cout<<endl;
+     }
+     
+     return 0;
+ }
